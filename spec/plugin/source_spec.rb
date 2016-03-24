51d13b43ee4dff200c9b43117711b5cde49a50d7
eugen0329 <eugen0329@gmail.com>
2016-03-24 20:18:16 +0300

    Minor fix

---
+++ spec/plugin/source_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+context 'esearch' do
+  context '#source' do
+    describe '#visual' do
+      it 'fails outside visualmode' do
+        # expect(expr("esearch#source#visual({'visualmode':1})"))
+      end
+    end
+  end
+end
