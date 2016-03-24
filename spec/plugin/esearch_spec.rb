67837e29de6918c664b3e24b6e951be50994861b
eugen0329 <eugen0329@gmail.com>
2016-03-24 21:33:51 +0300

    Fix spec to trigger type Enter prompt

--- spec/plugin/esearch_spec.rb
+++ spec/plugin/esearch_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 context 'esearch' do
+  let(:win_open_quota) { 4 }
+  
 
   it 'can be tested' do
     expect(has('clientserver')).to be_truthy
@@ -13,16 +15,27 @@ context 'esearch' do
       # press ':call esearch#init()<Enter>asd<Enter>'
 
       cmd("let g:esearch = esearch#opts#new(exists('g:esearch') ? g:esearch : {})")
-      cmd("let g:exp = { 'vim': 'asdasdasdasd', 'pcre': 'asdasdasdasd', 'literal': 'asdasdasdasd' }")
+      cmd("let g:exp = { 'vim': 'lorem_ipsum', 'pcre': 'lorem_ipsum', 'literal': 'lorem_ipsum' }")
       cmd("let g:exp = esearch#regex#finalize(g:exp, g:esearch)")
       puts cmd("try | call esearch#init({'exp': g:exp}) | catch | echo v:exception | endtry")
 
 
       puts expr('bufnr("$")')
-      expect { press("<Nop>");exists('b:esearch') }.to become_true_within(4.second)
-      expect { line(1) =~ /Finish/i }.to become_true_within(10.second)
-      expect(bufname("%")).to match(/Search/)
+
+      expected = expect do
+        press("<Nop>") # to skip "Press ENTER or type command to continue" prompt
+        exists('b:esearch')
+      end
+      expected.to become_true_within(win_open_quota.second),
+        "Expect ESearch win will be opened in #{win_open_quota}"
+
+      puts expr('b:esearch.cwd')
+      puts expr('$PWD')
       expect(expr('b:esearch.cwd')).to eq(expr('$PWD'))
+
+      puts expr('&ut')
+      expect { line(1) =~ /Finish/i }.to become_true_within(40.second)
+      expect(bufname("%")).to match(/Search/)
     end
 
     # it 'fails with adapter error' do
