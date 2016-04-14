7ee6d8e91a0b424d01867b847d41f335e4e35af6
eugen0329 <eugen0329@gmail.com>
2016-04-14 23:32:25 +0300

    Fix [] in filenames, add rough literal/regex specs

--- spec/plugin/shared_examples/backend.rb
+++ spec/plugin/shared_examples/backend.rb
@@ -13,22 +13,36 @@ RSpec.shared_examples 'a backend' do |backend, adapter|
         cmd('close!') if bufname("%") =~ /Search/
       end
 
-
-      File.readlines('spec/fixtures/backend/test.txt').map(&:chomp).each do |test_query|
-        it "properly escapes `#{test_query}`" do
-          press ":call esearch#init()<Enter>#{test_query}<Enter>"
-
-          expected = expect {
-            press("j") # preto skip "Press ENTER or type command to continue" prompt
-            bufname("%") =~ /Search/
-          }.to become_true_within(5.second)
-
-          expect { line(1) =~ /Finish/i }.to become_true_within(10.seconds),
-            -> { "Expected first line to match /Finish/, got `#{line(1)}`" }
-        end
+      context 'literal' do
+        settings_dependent_context('literal', regex: 0)
+      end
+      context 'regex' do
+        settings_dependent_context('regex', regex: 1)
       end
     end
   end
 
   include_context 'dumpable'
+end
+
+# TODO
+def settings_dependent_context(type, settings)
+  before :each do
+    esearch_settings(settings)
+  end
+
+  File.readlines("spec/fixtures/backend/#{type}.txt").map(&:chomp).each do |test_query|
+    it "finds `#{test_query}`" do
+      press ":call esearch#init()<Enter>#{test_query}<Enter>"
+
+      expected = expect {
+        press("j") # preto skip "Press ENTER or type command to continue" prompt
+        bufname("%") =~ /Search/
+      }.to become_true_within(5.second)
+
+      expect { line(1) =~ /Finish/i }.to become_true_within(10.seconds),
+        -> { "Expected first line to match /Finish/, got `#{line(1)}`" }
+    end
+  end
+
 end
