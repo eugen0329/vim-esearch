e6ce55ad2e96dbfbb1ac4c702ff48b046dd5d518
eugen0329 <eugen0329@gmail.com>
2017-04-27 02:33:56 +0300

    Minor fix

--- .ci/install_dependencies.sh
+++ .ci/install_dependencies.sh
@@ -1,8 +1,6 @@
-ORIG_DIR=`pwd`
-
 # Install vim deps
 git clone https://github.com/thinca/vim-prettyprint ./.dep/vim-prettyprint
 git clone https://github.com/Shougo/vimproc.vim     ./.dep/vimproc.vim
 cd ./.dep/vimproc.vim
 make
-cd $ORIG_DIR
+cd -
