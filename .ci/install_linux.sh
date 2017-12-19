728e09877ca19b3d7e7156740cf5b68dc2cc3e28
eugen0329 <eugen0329@gmail.com>
2017-12-19 16:43:02 +0300

    extract travis commands to .ci

---
+++ .ci/install_linux.sh
@@ -0,0 +1,3 @@
+add-apt-repository ppa:jonathonf/vim -y
+apt update -y
+apt-get install -y vim ack-grep silversearcher-ag
---
+++ .ci/install_osx.sh
@@ -0,0 +1,6 @@
+brew update
+brew install macvim --with-client-server
+brew install ack
+brew install the_silver_searcher
+brew install the_platinum_searcher
+brew install ripgrep
