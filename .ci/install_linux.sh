924efa847f74603c278b83840be23bca0fea3ba2
eugen0329 <eugen0329@gmail.com>
2017-12-19 22:30:59 +0300

    trying to fix ack in build for linux

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -1,3 +1,4 @@
 sudo add-apt-repository ppa:jonathonf/vim -y
 sudo apt update -y
 sudo apt-get install -y vim vim-gtk ack-grep silversearcher-ag
+sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
