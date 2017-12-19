fe16a4fe9c38d42bca889c3cf683e53d43fe6330
eugen0329 <eugen0329@gmail.com>
2017-12-19 22:59:26 +0300

    Minor fix

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -11,4 +11,4 @@ sudo mv ripgrep-0.7.1-x86_64-unknown-linux-musl/rg /usr/bin/rg
 # install pt
 wget https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.1.5/pt_linux_amd64.tar.gz
 tar xvfz pt_linux_amd64.tar.gz
-mv pt_linux_amd64/pt /usr/bin/pt
+sudo mv pt_linux_amd64/pt /usr/bin/pt
