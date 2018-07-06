e98ebba510b5418fad14b6f1548e2171af3f59e5
Sharayeu <Yauhen_Sharayeu@fidor.com>
2018-07-06 12:04:02 +0300

    Minor fix

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -1,10 +1,14 @@
 sudo add-apt-repository ppa:jonathonf/vim -y
 sudo apt update -y
 sudo apt-get install -y vim 
-sudo apt-get install -y vim-gtk
+sudo apt-get install -f -y vim-gtk
+
+# install ack
 sudo apt-get install -y ack-grep
-sudo apt-get install -y silversearcher-ag
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
+
+# install ag
+sudo apt-get install -y silversearcher-ag
 
 # install rg
 wget https://github.com/BurntSushi/ripgrep/releases/download/0.7.1/ripgrep-0.7.1-x86_64-unknown-linux-musl.tar.gz
