138bc6a014a242201588a21fed3b87e5b485112d
Sharayeu <Yauhen_Sharayeu@fidor.com>
2018-07-06 11:58:44 +0300

    Minor fix

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -1,6 +1,9 @@
 sudo add-apt-repository ppa:jonathonf/vim -y
 sudo apt update -y
-sudo apt-get install -y vim vim-gtk ack-grep silversearcher-ag
+sudo apt-get install -y vim 
+sudo apt-get install -y vim-gtk
+sudo apt-get install -y ack-grep
+sudo apt-get install -y silversearcher-ag
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 
 # install rg
