875feee38893b8ae4d3d6f44f3b69d40bfed1d25
eugen0329 <eugen0329@gmail.com>
2019-07-20 23:55:39 +0300

    Minor fix

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -4,5 +4,5 @@ bundle install
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then bash .ci/install_osx.sh; fi
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then ( sudo Xvfb :99 -ac -screen 0 1024x768x8; echo ok ) & fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then bash .ci/install_linux.sh; fi
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then DISPLAY=:99.0 sh -e /etc/init.d/xvfb start; fi 
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then DISPLAY=":99.0" sh -e /etc/init.d/xvfb start; fi 
 sleep 5 # give xvfb some time to start
