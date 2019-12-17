03f1bea067992be31b7e83fe41b5f02e4721aa94
eugen0329 <eugen0329@gmail.com>
2019-12-18 01:35:58 +0300

    put backend specs into a single file

--- spec/plugin/backend_spec.rb
+++ spec/plugin/backend_spec.rb
@@ -5,6 +5,10 @@ require 'plugin/shared_contexts/dumpable.rb'
 
 context 'esearch' do
   context '#backend' do
+    describe '#system', :system do
+      it_behaves_like 'a backend', 'system'
+    end
+
     describe '#vimproc', :vimproc do
       before(:all) do
         press ":let g:esearch#backend#vimproc#updatetime = 30"
@@ -13,6 +17,13 @@ context 'esearch' do
 
       it_behaves_like 'a backend', 'vimproc'# if ENV['TRAVIS_OS_NAME'] != 'osx'
       it_behaves_like 'an abortable backend', 'vimproc'
+    end
+
+    describe '#vim8', :vim8 do
+      before { press ":let g:esearch#backend#vim8#timer = 100<Enter>" }
+
+      it_behaves_like 'a backend', 'vim8'
+      it_behaves_like 'an abortable backend', 'vim8'
     end
   end
 end
