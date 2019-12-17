2418c6bbf51bceee199f00edb4047701d09f1193
eugen0329 <eugen0329@gmail.com>
2019-12-18 02:26:29 +0300

    Minor fix

--- .ci/before_script.sh
+++ .ci/before_script.sh
@@ -2,6 +2,6 @@ bash .ci/install_vim_dependencies.sh
 bundle install
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then bash .ci/install_osx.sh; fi
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then ( sudo Xvfb :99 -ac -screen 0 1024x768x8; echo ok ) &; sleep 5 fi
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then ( sudo Xvfb :99 -ac -screen 0 1024x768x8; echo ok ) &; sleep 5; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then bash .ci/install_linux.sh; fi
 # if [ "$TRAVIS_OS_NAME" = "linux" ]; then DISPLAY=":99.0" sh -e /etc/init.d/xvfb start; sleep 5 fi 
