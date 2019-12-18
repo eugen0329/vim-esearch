0e3d88d554e0f9857d1ff0e26b7891d0cf309e23
eugen0329 <eugen0329@gmail.com>
2019-12-18 12:06:02 +0300

    Revert "move travis wait to .ci/install_osx.sh"
    This reverts commit 177b809ea9afcf4b85766db37be3bfbcf93eef20.

--- .ci/install_osx.sh
+++ .ci/install_osx.sh
@@ -3,5 +3,5 @@ brew install macvim --with-client-server
 brew install ack
 brew install the_silver_searcher
 brew install the_platinum_searcher
-travis_wait 60 brew install ripgrep
+brew install ripgrep
 brew reinstall --with-pcre git
