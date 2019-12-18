491c6dc96829f066956796b8179346f0874f1c65
eugen0329 <eugen0329@gmail.com>
2019-12-18 14:56:49 +0300

    override system vim in osx build

--- .ci/install_osx.sh
+++ .ci/install_osx.sh
@@ -1,5 +1,5 @@
 brew update
-brew install macvim --with-client-server
+brew install macvim --with-override-system-vim
 brew install ack
 brew install the_silver_searcher
 brew install the_platinum_searcher
