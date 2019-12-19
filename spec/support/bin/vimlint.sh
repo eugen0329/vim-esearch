31dbca08c5c61d7fbc66e459a29711f2a63ec3e1
eugen0329 <eugen0329@gmail.com>
2019-12-20 01:07:44 +0300

    fix shellchek recommendations

--- spec/support/bin/vimlint.sh
+++ spec/support/bin/vimlint.sh
@@ -3,13 +3,15 @@
 vimlint="$1"
 vimlparser="$2"
 shift 2
+directories=$*
 
 # VIMLINT_IGNORE_JOB_CONTROL_WARNINGS are ignored.  TODO figure out why,
 # until that VIMLINT_WARNING_UNUSED_ARGUMENTS_AND_VARIABLES_WORKAROUND
 # will be used. Seems, similar problem happens with
 # VIMLINT_IGNORE_UNDERSCORED_VARIABLE_NAME
-vimlint_ignore_job_control_warnings='-e EVL103.a:channel=1 -e EVL103.a:event=1 -e EVL103.a:job=1 -e EVL103.a:pty=1 -e EVL103.a:timer=1'
-vimlint_ignore_underscored_variable_name='-e EVL102.l:_=1'
+
+# vimlint_ignore_job_control_warnings='-e EVL103.a:channel=1 -e EVL103.a:event=1 -e EVL103.a:job=1 -e EVL103.a:pty=1 -e EVL103.a:timer=1'
+# vimlint_ignore_underscored_variable_name='-e EVL102.l:_=1'
 
 vimlint_warning_unused_arguments_and_variables_workaround='-e EVL103=3'
 todo_vimlint_warning_unused_local_variables='-e EVL102=3'
@@ -20,14 +22,14 @@ sh "$vimlint/bin/vimlint.sh" -l "$vimlint" -p "$vimlparser"
   $vimlint_warning_unused_arguments_and_variables_workaround
   $todo_vimlint_warning_unused_local_variables
   $todo_vimlint_warning_scipt_encodings
-  $@
+  $directories
 EOF
 )
-
+# shellcheck disable=SC2086
 output=$(eval $command)
 status=$?
-echo -e "$output"
+printf "$output\n"
 
 [ $status -eq 0 ] && exit 0 # if no errors or warnings
-echo -e $output | grep --color ":Error:" && exit 1 # Allow warnings but not errors
+printf "$output\n" | grep --color ":Error:" && exit 1 # Allow warnings but not errors
 exit 0
