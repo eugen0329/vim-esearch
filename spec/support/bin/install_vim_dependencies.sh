8b730955b672bb27161168a49e67d59339cf03ac
eugen0329 <eugen0329@gmail.com>
2019-12-20 18:30:10 +0300

    lower nvim version

--- spec/support/bin/install_vim_dependencies.sh
+++ spec/support/bin/install_vim_dependencies.sh
@@ -39,7 +39,7 @@ git -C "$plugins_directory/vimproc.vim" checkout 89065f62883edb10a99aa1b1640d6d4
 (cd "$plugins_directory/vimproc.vim" && make)
 
 # Download neovim
-wget -N https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P "$bin_directory"
+wget -N https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage -P "$bin_directory"
 chmod +x "$bin_directory/nvim.appimage"
 
 pip3 install neovim-remote
