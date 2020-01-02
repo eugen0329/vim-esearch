41fafcbb317e02d0513fd844a4636e8029940529
eugen0329 <eugen0329@gmail.com>
2020-01-03 01:22:31 +0300

    disable neovim isntall for debian

--- spec/support/provision/debian.sh
+++ spec/support/provision/debian.sh
@@ -4,10 +4,9 @@
 . "$(dirname "$0")/__provision.sh"
 local_directory_path="${1:-"$provision_directory/../bin"}"
 
-echo $create_link_to_default_in_local_directory
-# install_package_vim       \
-#   'latest'                \
-#   "$use_sudo"
+install_package_vim       \
+  'latest'                \
+  "$use_sudo"
 
 # install_prebuilt_neovim   \
 #   "0.4.3"                 \
@@ -15,25 +14,25 @@ echo $create_link_to_default_in_local_directory
 #   "$skip_global_install"  \
 #   "$create_link_to_default_in_local_directory"
 
-# install_prebuilt_pt       \
-#   '2.2.0'                 \
-#   "$local_directory_path" \
-#   "$skip_global_install"  \
-#   "$create_link_to_default_in_local_directory"
+install_prebuilt_pt       \
+  '2.2.0'                 \
+  "$local_directory_path" \
+  "$skip_global_install"  \
+  "$create_link_to_default_in_local_directory"
 
-# install_package_ack       \
-#   'latest'                \
-#   "$use_sudo"
+install_package_ack       \
+  'latest'                \
+  "$use_sudo"
 
-# install_package_ag        \
-#   'latest'                \
-#   "$use_sudo"
+install_package_ag        \
+  'latest'                \
+  "$use_sudo"
 
-# install_prebuilt_rg       \
-#   '11.0.2'                \
-#   "$local_directory_path" \
-#   "$skip_global_install"  \
-#   "$create_link_to_default_in_local_directory"
+install_prebuilt_rg       \
+  '11.0.2'                \
+  "$local_directory_path" \
+  "$skip_global_install"  \
+  "$create_link_to_default_in_local_directory"
 
 # # shellcheck source=spec/support/provision/plugins.sh
-# sh "$provision_directory/default_plugins.sh" "${2:-"$provision_directory/../vim_plugins"}"
+sh "$provision_directory/default_plugins.sh" "${2:-"$provision_directory/../vim_plugins"}"
