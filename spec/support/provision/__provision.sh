9ac2a202ce99cf6b83247a53ada601d2b9fdad72
eugen0329 <eugen0329@gmail.com>
2020-01-02 22:10:16 +0300

    refactor lib, fix shellcheck warnings

--- spec/support/provision/__provision.sh
+++ spec/support/provision/__provision.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-. "$(dirname $0)/__vim.sh"
-. "$(dirname $0)/__neovim.sh"
-. "$(dirname $0)/__ack.sh"
-. "$(dirname $0)/__ag.sh"
-. "$(dirname $0)/__pt.sh"
-. "$(dirname $0)/__rg.sh"
+. "$(dirname "$0")/__vim.sh"
+. "$(dirname "$0")/__neovim.sh"
+. "$(dirname "$0")/__ack.sh"
+. "$(dirname "$0")/__ag.sh"
+. "$(dirname "$0")/__pt.sh"
+. "$(dirname "$0")/__rg.sh"
 
-. "$(dirname $0)/__lib.sh"
+. "$(dirname "$0")/__lib.sh"
 
-provision_directory="$(dirname "$(crossplatform_realpath "$0")")"
-bin_directory="${1:-"$(dirname "$(crossplatform_realpath "$0")")/../bin"}"
 mkdir -p "$bin_directory"
 set -eux
