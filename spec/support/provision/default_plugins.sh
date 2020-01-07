5d952328dc3a34dbe02986eb8766dab5ef876a61
eugen0329 <eugen0329@gmail.com>
2020-01-07 16:05:27 +0300

    write tests for plugins installation

--- spec/support/provision/default_plugins.sh
+++ spec/support/provision/default_plugins.sh
@@ -8,12 +8,10 @@ plugins_directory="${1:-"$provision_directory/../vim_plugins"}"
 install_vim_plugin                            \
   'https://github.com/Shougo/vimproc.vim'     \
   "$plugins_directory/vimproc.vim"            \
-  'master'                                    \
   '89065f62883edb10a99aa1b1640d6d411907316b'  \
   "make"
 
 install_vim_plugin                            \
   'https://github.com/thinca/vim-prettyprint' \
   "$plugins_directory/vim-prettyprint"        \
-  'master'                                    \
   'd6060d2b1ff1cff71714e126addd3b10883ade12'
--- spec/support/provision/dependencies/plugins.sh
+++ spec/support/provision/dependencies/plugins.sh
@@ -7,20 +7,18 @@ build_command_noop=''
 
 # TODO idempotence
 install_vim_plugin() {
-  local repository_url="$1"
-  local clone_path="$2"
-  local branch="${3:-"$pull_all_branches"}"
-  local commit_hash="${4:-"$dont_checkout"}"
-  local build_commands="${5:-"$build_command_noop"}"
+  local repo="$1"
+  local dest="$2"
+  local version="${3:-"$dont_checkout"}"
+  local build_commands="${4:-"$build_command_noop"}"
 
   git_clone_and_checkout \
-    "$repository_url"    \
-    "$clone_path"        \
-    "$branch"            \
-    "$commit_hash"
+    "$repo"              \
+    "$dest"              \
+    "$version"
 
   if [ "$build_commands" != "$build_command_noop" ]; then
-    ( cd "$clone_path" && eval "$build_commands" )
+    ( cd "$dest" && eval "$build_commands" )
   fi
 }
 
