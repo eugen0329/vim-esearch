d919d59aca56a1456772e4cad9083d3499a35514
eugen0329 <eugen0329@gmail.com>
2020-01-08 17:43:50 +0300

    add shellcheck script to run from docker

--- spec/support/scripts/shellcheck.bash
+++ spec/support/scripts/shellcheck.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Required for travis as globbing isn't working in .travis.yml and .shellcheckrc
-# is not working
+# Mostly for convenience as .shellcheckrc is not working for some reason
 
-# --external-sources: Allow 'source' outside of FILES
-exec shellcheck -e SC2039 --external-sources \
-    spec/support/**/*.sh                     \
-    spec/support/**/*.bash                   \
-    spec/support/**/*.bats                   \
-    spec/support/**/build*                   \
-    spec/support/**/run*                     \
-    spec/support/docker/rspec*               \
-    spec/support/provision/test/bats
+shopt -s globstar
+
+all=(spec/support/**/*.sh                    \
+     spec/support/**/*.bash                  \
+     spec/support/**/*.bats                  \
+     spec/support/**/build*                  \
+     spec/support/**/run*                    \
+     spec/support/**/bats                    \
+     spec/support/docker/{shellcheck,rspec}* \
+     )
+
+echo -e "Running shellcheck for file(s): ${*:-${all[*]}}"
+shellcheck -e SC2039 --external-sources "${@:-${all[@]}}"
