f2781a31f2e3f7c4b353c852a67725eea8212623
eugen0329 <eugen0329@gmail.com>
2020-01-19 00:13:52 +0300

    Minor fix

--- spec/support/subscriptions.rb
+++ spec/support/subscriptions.rb
@@ -5,19 +5,19 @@ require 'active_support/notifications'
 LOG_TAG_PADDING = 15
 
 ActiveSupport::Notifications.subscribe(/cache_read/) do |_name, _start, _finish, _id, payload|
-  Configuration.log.tagged('cache.read'.rjust(LOG_TAG_PADDING) do
+  Configuration.log.tagged('cache.read'.rjust(LOG_TAG_PADDING)) do
     Configuration.log.debug { (payload[:key]).to_s }
   end
 end
 
 ActiveSupport::Notifications.subscribe(/cache_clear/) do |_name, _start, _finish, _id, payload|
-  Configuration.log.tagged('cache.clear'.rjust(LOG_TAG_PADDING) do
+  Configuration.log.tagged('cache.clear'.rjust(LOG_TAG_PADDING)) do
     Configuration.log.debug { "CLEAR #{payload[:object_id]}" }
   end
 end
 
 ActiveSupport::Notifications.subscribe(/cache_write_value/) do |_name, _start, _finish, _id, payload|
-  Configuration.log.tagged('cache.write'.rjust(LOG_TAG_PADDING) do
+  Configuration.log.tagged('cache.write'.rjust(LOG_TAG_PADDING)) do
     Configuration.log.debug do
       echos =
         if Configuration.debug_specs_performance?
@@ -32,7 +32,7 @@ ActiveSupport::Notifications.subscribe(/cache_write_value/) do |_name, _start, _
 end
 
 ActiveSupport::Notifications.subscribe(/\Aeditor\./) do |name, _start, _finish, _id, payload|
-  Configuration.log.tagged(name.rjust(LOG_TAG_PADDING) do
+  Configuration.log.tagged(name.rjust(LOG_TAG_PADDING)) do
     Configuration.log.debug { (payload[:data]).to_s }
   end
 end
