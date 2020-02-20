0bd7b13512bc4132ec773a8336aca92ca3bdd328
eugen0329 <eugen0329@gmail.com>
2020-02-20 17:29:08 +0300

    more explicit root for autoload paths

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,7 +26,10 @@ Configuration.tap do |c|
   c.process_check_timeout = 10.second
 end
 
-ActiveSupport::Dependencies.autoload_paths += ['spec/support', 'spec/support/lib']
+ActiveSupport::Dependencies.autoload_paths += [
+  Configuration.root.join('spec/support'),
+  Configuration.root.join('spec/support/lib'),
+]
 require 'support/client'
 require 'support/server'
 
