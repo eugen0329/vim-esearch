332eb38cacb1bde1ceebf24f928a803d0a298f57
eugen0329 <eugen0329@gmail.com>
2020-02-20 17:50:49 +0300

    Revert "trying to move everything out from lib"
    This reverts commit 2257e56dd1c739576228b1ae9a8f586b19f8d17b.

--- spec/support/lib/viml_value.rb
+++ spec/support/lib/viml_value.rb
@@ -4,16 +4,16 @@ module VimlValue
   class ParseError < RuntimeError; end
 
   def self.load(string, allow_toplevel_literals: false)
-    tree = VimlValue::Parser
-           .new(VimlValue::Lexer.new(string), allow_toplevel_literals: allow_toplevel_literals)
+    tree = Parser
+           .new(Lexer.new(string), allow_toplevel_literals: allow_toplevel_literals)
            .parse
 
     return tree if tree.nil?
 
-    VimlValue::Visitors::ToRuby.new.accept(tree)
+    Visitors::ToRuby.new.accept(tree)
   end
 
-  def self.dump(object, visitor: VimlValue::Visitors::ToVim)
+  def self.dump(object, visitor: Visitors::ToVim)
     visitor.new.accept(object)
   end
 end
