37400ba7d37f654f5a8a78168f93bbc8722ef583
eugen0329 <eugen0329@gmail.com>
2020-09-04 22:20:25 +0300

    fix vim lua parser

--- lua/esearch/vim/parse.lua
+++ lua/esearch/vim/parse.lua
@@ -8,19 +8,19 @@ function M.lines(data)
   local separators_count = 0
   -- must be invalidated across calls to prevent using stale file presence information
   local cache = {}
-  local filename, lnum, text, git
+  local filename, lnum, text, rev
 
   for line in data() do
     if line:len() == 0 or line == '--' then
       separators_count = separators_count + 1
     else
-      filename, lnum, text, git = parse_line(line, cache)
+      filename, lnum, text, rev = parse_line(line, cache)
       if filename then
         parsed:add(vim.dict({
           filename = filename,
           lnum     = lnum,
           text     = text:gsub("[\r\n]", ''),
-          git      = git,
+          rev      = rev,
         }))
       end
     end
